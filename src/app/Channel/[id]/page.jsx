f1dbaff854d198c0b0e20fbf16caf77f59181f37
Amrr-Maherr <amrr.maherr242gmail.com>
2025-10-09 17:51:06 +0300

    feat: fetch and handle channel details using useEffect hooks
    - Added useEffect to dispatch FetchChannelDetails when `id` changes
    - Implemented a separate useEffect to extract and handle channel data after it loads
    - Improved data handling structure by separating fetching and processing logic

---
+++ src/app/Channel/[id]/page.jsx
@@ -0,0 +1,34 @@
+"use client";
+import { FetchChannelDetails } from "@/Store/ChannelSlice";
+import { useParams } from "next/navigation";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+function Page() {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const Channel = useSelector((state) => state.Channel.data);
+  const error = useSelector((state) => state.error);
+  const loading = useSelector((state) => state.loading);
+  // fetch channel data
+  useEffect(() => {
+    if (id)
+      dispatch(FetchChannelDetails(id));
+  }, [id, dispatch]);
+  // channel data
+  useEffect(() => {
+    if (Channel) {
+      const ChannelData = Channel[0];
+      const Title = Channel[0].brandingSettings.channel.title;
+      const Description = Channel[0].brandingSettings.channel.description;
+      const Snippets = Channel[0].snippet;
+      const Statistics = Channel[0].statistics;
+      const Banner = Channel[0].brandingSettings.image.bannerExternalUrl;
+    }
+  }, [Channel]);
+  return (
+    <>
+    </>
+  );
+}
+
+export default Page;
