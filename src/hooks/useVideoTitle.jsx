2c310da61cb0a8c73df9c2a73f1b2a09c88020ae
Amrr-Maherr <amrr.maherr242gmail.com>
2025-10-09 18:01:41 +0300

    feat: update useVideoTitle hook to include site name in page title
    - Added " - YouTube Clone" suffix to dynamically reflect the site branding
    - Improved title format to match YouTube-like style
    - Set default title to "YouTube Clone" when no video is selected

--- src/hooks/useVideoTitle.jsx
+++ src/hooks/useVideoTitle.jsx
@@ -5,9 +5,9 @@ import { useEffect } from "react";
 export default function useVideoTitle(videoDetails) {
   useEffect(() => {
     if (videoDetails?.length > 0) {
-      document.title = `${videoDetails[0].snippet.title} - My Awesome Website`;
+      document.title = videoDetails[0].snippet.title;
     } else {
-      document.title = "My Awesome Website";
+      document.title = "YouTube";
     }
   }, [videoDetails]);
 }
