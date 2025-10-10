a7b65375ff3c9c328d87d7035c478620b112d1ac
Amrr-Maherr <amrr.maherr242gmail.com>
2025-10-10 18:02:29 +0300

    update

--- src/components/VideoCard/Index.jsx
+++ src/components/VideoCard/Index.jsx
@@ -6,8 +6,9 @@ import VideoActions from "./Elements/VideoActions";
 
 function Index({ video }) {
   const videoId =
-    video.id?.videoId ||
-    video.contentDetails?.videoId
+    video.id?.videoId || // search results
+    video.contentDetails?.videoId || // playlist items
+    (typeof video.id === "string" ? video.id : null); // fallback
   return (
     <Link href={`/VideoDetails/${videoId}`}>
       <div className="w-full flex-shrink-0 flex-col mb-5 cursor-pointer mx-auto">
