e9c22c701d00228fc024ce126e11c8f3f8bd8d77
Amrr-Maherr <amrr.maherr242gmail.com>
2025-12-05 15:32:17 +0200

    update

---
+++ src/app/SearchResult/SearchResultClient.jsx
@@ -0,0 +1,25 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import { useSearchVideos } from "@/hooks/useSearchVideos";
+import SearchResultCard from "./Elements/SearchResultCard";
+import SearchResultCardLoader from "@/components/LoadingSkeleton/SearchResultCardLoader";
+
+function SearchResultClient() {
+  const searchParams = useSearchParams();
+  const query = searchParams.get("q");
+  const { data: SearchResult, isLoading: loading } = useSearchVideos(query);
+
+  if (loading) {
+    return <SearchResultCardLoader />;
+  }
+
+  return (
+    <section className="container mx-auto">
+      {SearchResult?.map((video) => (
+        <SearchResultCard video={video} key={video.id.videoId} />
+      ))}
+    </section>
+  );
+}
+
+export default SearchResultClient;
--- src/app/SearchResult/page.jsx
+++ src/app/SearchResult/page.jsx
@@ -1,24 +1,12 @@
 "use client";
-import { useSearchParams } from "next/navigation";
-import { useSearchVideos } from "@/hooks/useSearchVideos";
-import SearchResultCard from "./Elements/SearchResultCard";
-import SearchResultCardLoader from "@/components/LoadingSkeleton/SearchResultCardLoader";
+import React from "react";
+import SearchResultClient from "./SearchResultClient";
 
 function Page() {
-  const searchParams = useSearchParams();
-  const query = searchParams.get("q");
-  const { data: SearchResult, isLoading: loading } = useSearchVideos(query);
-
-  if (loading) {
-    return <SearchResultCardLoader />;
-  }
-
   return (
-    <section className="container mx-auto">
-      {SearchResult?.map((video) => (
-        <SearchResultCard video={video} key={video.id.videoId} />
-      ))}
-    </section>
+    <React.Suspense fallback={<div>Loading search results...</div>}>
+      <SearchResultClient />
+    </React.Suspense>
   );
 }
 
